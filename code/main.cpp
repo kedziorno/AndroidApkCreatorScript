9e0f2ebf16a9483d724527c9846f97999a9f0654
kedziorno <kjoblak85@gmail.com>
2020-02-20 16:02:35 +0100

    worked version - ox x86_64

---
+++ code/main.cpp
@@ -0,0 +1,6 @@
+#include "main.hpp"
+
+int main(void) {
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
---
+++ code/main.hpp
@@ -0,0 +1,6 @@
+#ifndef MAIN_HPP_C
+#define MAIN_HPP_C
+
+#include <iostream>
+
+#endif
